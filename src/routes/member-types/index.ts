2f7b04c0f76882d9bd3945a9cc867cb39b23ccf0
Jenyajk <101403726+Jenyajk@users.noreply.github.com>
2023-05-29 22:58:19 +0500

    feat: add logic for routes/member-types

--- src/routes/member-types/index.ts
+++ src/routes/member-types/index.ts
@@ -6,9 +6,9 @@ import type { MemberTypeEntity } from '../../utils/DB/entities/DBMemberTypes';
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
   fastify
 ): Promise<void> => {
-  fastify.get('/', async function (request, reply): Promise<
-    MemberTypeEntity[]
-  > {});
+  fastify.get('/', async function (request, reply): Promise<MemberTypeEntity[]> {
+    return fastify.db.memberTypes.findMany()
+  });
 
   fastify.get(
     '/:id',
@@ -17,7 +17,16 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<MemberTypeEntity> {}
+    async function (request, reply): Promise<MemberTypeEntity> {
+      try {
+        const res = await fastify.db.memberTypes.findOne({ key: 'id', equals: request.params.id });
+        return res ? res : reply.code(404).send({ message: 'Not found' });
+      } catch (error) {
+        console.error(error);
+        reply.code(500).send({ message: 'Internal Server Error' });
+        throw error;
+      }
+    }
   );
 
   fastify.patch(
@@ -28,7 +37,14 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<MemberTypeEntity> {}
+    async function (request, reply): Promise<MemberTypeEntity> {
+      try {
+        return fastify.db.memberTypes.change(request.params.id, request.body);
+      } catch (error) {
+        reply.code(400).send({ message: (error as Error).message ?? 'Bad Request' });
+        throw error;
+      }
+    }
   );
 };
 
